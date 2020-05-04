ee52df8d9db143e744db020222b4d5fa60cb98ec
Andres <agallardoysla@gmail.com>
2020-05-04 08:37:21 -0500

    init

--- production.js
+++ production.js
@@ -4,7 +4,7 @@ const options = {};
 //options.ip = "http://localhost";
 options.port = process.env.PORT || 5000;
 
-options.ip = "localhost";
+options.ip = "179.6.38.209";
 //options.port = parseInt(process.argv[2]);
 // options.config = { name: 'W2API.js' };
 // options.sleep = 3000;
